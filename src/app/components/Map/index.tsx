5ba16728d097efc5f5f930568adecc40eaac3133
Papidev <lzanzottera@gmail.com>
2025-11-22 03:32:23 +0100

    Prettier format on current files

--- src/app/components/Map/index.tsx
+++ src/app/components/Map/index.tsx
@@ -1,39 +1,37 @@
 "use client";
 
-import {useEffect, useRef} from "react";
-import maplibregl, {Map as MapLibreMap} from "maplibre-gl";
+import { useEffect, useRef } from "react";
+import maplibregl, { Map as MapLibreMap } from "maplibre-gl";
 import "maplibre-gl/dist/maplibre-gl.css";
 
 type Props = {
-    coordinates: [number, number];
-    zoom: number;
+  coordinates: [number, number];
+  zoom: number;
 };
 
-export const Map = ({coordinates,zoom}:Props) => {
-    const mapContainerRef = useRef<HTMLDivElement | null>(null);
-    const mapRef = useRef<MapLibreMap | null>(null);
+export const Map = ({ coordinates, zoom }: Props) => {
+  const mapContainerRef = useRef<HTMLDivElement | null>(null);
+  const mapRef = useRef<MapLibreMap | null>(null);
 
-    useEffect(() => {
-        if (!mapContainerRef.current || mapRef.current) {
-            return
-        }
+  useEffect(() => {
+    if (!mapContainerRef.current || mapRef.current) {
+      return;
+    }
 
-        const map = new maplibregl.Map({
-            container: mapContainerRef.current,
-            style: "https://demotiles.maplibre.org/style.json",
-            center: coordinates,
-            zoom,
-        });
+    const map = new maplibregl.Map({
+      container: mapContainerRef.current,
+      style: "https://demotiles.maplibre.org/style.json",
+      center: coordinates,
+      zoom,
+    });
 
-        map.addControl(new maplibregl.NavigationControl(), "top-right");
-        mapRef.current = map;
+    map.addControl(new maplibregl.NavigationControl(), "top-right");
+    mapRef.current = map;
 
-        return () => {
-            map.remove();
-        };
-    }, [coordinates,zoom]);
+    return () => {
+      map.remove();
+    };
+  }, [coordinates, zoom]);
 
-    return (
-        <div ref={mapContainerRef} className="w-full h-full" />
-    );
+  return <div ref={mapContainerRef} className="w-full h-full" />;
 };
--- src/app/components/RomeMap.tsx
+++ src/app/components/RomeMap.tsx
@@ -1,13 +1,7 @@
 "use client";
 
-import {Map} from '@/app/components/Map'
-
+import { Map } from "@/app/components/Map";
 
 export const RomeMap = () => {
-
-
-    return (
-
-        <Map coordinates={[12.4964, 41.9028]} zoom={10}/>
-    );
+  return <Map coordinates={[12.4964, 41.9028]} zoom={10} />;
 };
