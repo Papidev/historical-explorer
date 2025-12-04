604f15248876f49dff3a024344ffcfa5f1efd101
Papidev <lzanzottera@gmail.com>
2025-12-04 20:12:54 +0100

    Add link to explore Rome map on the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+      <main
+        className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+        <Link
+          className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
+          href="/rome"
+        >
+          Explore Rome Map
+        </Link>
         <Image
           className="dark:invert"
           src="/next.svg"
@@ -58,6 +66,7 @@ export default function Home() {
           >
             Documentation
           </a>
+
         </div>
       </main>
     </div>
